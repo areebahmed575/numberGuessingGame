0e1e0ce56230dc8e329584d5a05540971e1f2f2a
areebahmed575 <areebahmed575@gmail.com>
2023-02-14 01:36:39 +0500

    add numberGuessingGame

---
+++ numberguess.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+let computerGuess = Math.floor(Math.random() * 10);
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "GuessNumber",
+        message: "Guess a number:"
+    }
+]);
+while (answer.GuessNumber != computerGuess) {
+    message: "Guess a number!!";
+    if (answer.GuessNumber == computerGuess) {
+        console.log("You won!");
+    }
+    else if (answer.GuessNumber >= computerGuess && answer.GuessNumber < 10) {
+        console.log("You have guess a greater number");
+    }
+    else if (answer.GuessNumber <= computerGuess && answer.GuessNumber > 0) {
+        console.log("You have guess a lesser number");
+    }
+    else {
+        console.log("enter a number b/w 1 to 10");
+    }
+}
